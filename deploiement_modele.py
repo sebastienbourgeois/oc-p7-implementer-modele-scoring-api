3e51c731ca7ada3b09b6ef28a1b013263d2c7dde
Sebastien Bourgeois <sebastienbourgeois60@gmail.com>
2022-05-12 22:18:21 +0200

    Création du repository

---
+++ deploiement_modele.py
@@ -0,0 +1,25 @@
+import pickle
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Récupération du modèle pour sortir les prédictions
+modele = pickle.load(open("modele.pkl", "rb"))
+
+# Prédit si le client aura des problèmes de remboursement et le score échéant
+@app.route('/predictions', methods=['POST'])
+def predire_octroi_score_credit():
+	# Récupération des données reçues via la requête
+	query_parameters = request.get_json()
+	std_donnees_client = query_parameters['std_donnees_client']
+ 
+ 	# Préparation de la réponse à envoyer
+	predictions_client = {}
+	predictions_client['problemes_remboursement'] = modele.predict(std_donnees_client).tolist()[0]
+	predictions_client['score_remboursement_client'] = modele.predict_proba(std_donnees_client).tolist()[0][0]
+
+	# Retourne la réponse au format json
+	return jsonify(predictions_client)
+
+if __name__ == "__main__":
+	app.run(debug=True)
